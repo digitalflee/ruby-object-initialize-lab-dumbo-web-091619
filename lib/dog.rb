aa6e740fbb815670a7051e0a057be83c5a8ca41d
Keenan Alves <keenan.alves@gmail.com>
2019-09-18 15:44:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,20 +6,5 @@ class Dog
     @breed = breed
   end 
   
-  # def name=(name) 
-  #   @name
-  # end 
-  
-  # def name 
-  #   @name 
-  # end 
-  
-  # def breed=(breed = "Mutt")
-  #   @breed = breed 
-  # end 
-  
-  # def breed 
-  #   @breed
-  # end 
 end 
 
