bf34760f2d1571c0f81ddfac8f7368f5b63d9ddf
Keenan Alves <keenan.alves@gmail.com>
2019-09-18 15:43:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,24 +2,24 @@
 
 class Dog 
   def initialize(name, breed)
-    @name
-    @breed 
+    @name = name
+    @breed = breed
   end 
   
-  def name=(name) 
-    @name
-  end 
+  # def name=(name) 
+  #   @name
+  # end 
   
-  def name 
-    @name 
-  end 
+  # def name 
+  #   @name 
+  # end 
   
-  def breed=(breed = "Mutt")
-    @breed = breed 
-  end 
+  # def breed=(breed = "Mutt")
+  #   @breed = breed 
+  # end 
   
-  def breed 
-    @breed
-  end 
+  # def breed 
+  #   @breed
+  # end 
 end 
 
